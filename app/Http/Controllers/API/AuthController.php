51094152d1bc8d2ee56b750cc75c99a38e6f9c9e
ksamrat224 <ksamrat224@gmail.com>
2025-06-29 18:51:26 +0545

    login authentication completed

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -37,7 +38,37 @@ class AuthController extends Controller
         ], 200);
     }
      public function login(Request $request) {
-        
+        $validateUser = Validator::make(
+        $request->all(),
+        [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]
+      );
+        if ($validateUser->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Authentication Error',
+                'errors' => $validateUser->errors()->all(),
+            ], 404);
+        }
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'status' => true,
+                'message' => 'User logged in successfully',
+                'user' => $user,
+                'token' => $token,
+                'token_type' => 'Bearer',
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => 'Invalid credentials',
+                'errors' =>$validateUser->errors()->all(),
+            ], 401);
+        }
     }
      public function logout(Request $request) {
         

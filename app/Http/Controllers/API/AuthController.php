f6551a41afaf5b7177df297ef9bfa5f0f480132c
ksamrat224 <ksamrat224@gmail.com>
2025-06-29 18:42:42 +0545

    signup authentication completed

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,10 +2,44 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function signup(Request $request) {
+      $validateUser = Validator::make(
+        $request->all(),
+        [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]
+      );
+      if ($validateUser->fails()) {
+          return response()->json([
+              'status' => false,
+              'message' => 'Validation Error',
+              'errors' => $validateUser->errors()->all(),
+          ], 401);
+      }
+      $user= User::create([
+          'name' => $request->name,
+          'email' => $request->email,
+          'password' =>$request->password,
+      ]);
+        return response()->json([
+            'status' => true,
+            'message' => 'User created successfully',
+            'user' => $user,
+        ], 200);
+    }
+     public function login(Request $request) {
+        
+    }
+     public function logout(Request $request) {
+        
+    }
 }
